f93054fc3cd7b3949ae5bf528bccbaac5a8c17f9
Amr Dawood <amr.dawood1@gmail.com>
2015-04-04 13:07:00 -0700

    Entry constructor added

--- src/rtreepack/Entry.java
+++ src/rtreepack/Entry.java
@@ -5,6 +5,12 @@ public class Entry
 	Node node;
 	int lsnExpected;
 	
+	public Entry(Node node, int lsnExpected) 
+	{
+		this.node = node;
+		this.lsnExpected = lsnExpected;
+	}
+
 	public String toString() 
 	{
 		return "Entry [node=" + node + ", lsnExpected=" + lsnExpected + "]";
